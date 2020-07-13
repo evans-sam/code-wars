fc55ff188e2b95030af188478ad07ba76733967b
Sam Evans <evansst@icloud.com>
2020-07-13 08:47:46 -0600

    Completes solution.

---
+++ ruby/071320.rb
@@ -0,0 +1,13 @@
+def disemvowel(str)
+  array = str.split(//)
+  new_array = []
+  vowels = ['a','e','i','o','u']
+  array.map do |letter|
+    new_array << letter if  !is_vowel_array(letter)  
+  end
+  new_array.join
+end
+
+def is_vowel_array(letter)
+  ["a", "e", "i", "o", "u"].include?(letter.downcase)
+end
