8b14d06d573e10355dfdbbce4fcef08f6c56a1fd
Sam Evans <evansst@icloud.com>
2020-07-13 11:35:55 -0600

    Adds dig_pow solution

--- ruby/071320.rb
+++ ruby/071320.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def disemvowel str
   str.delete('aeiouAEIOU')
@@ -11,4 +12,14 @@ end
 
 def find_outlier(integers)
   integers.count(&:even?) > 1 ? integers.find(&:odd?) : integers.find(&:even?)
+end
+
+def dig_pow(n, p)
+  n_array = n.to_s.chars.map(&:to_i)
+  result = n_array.map do |n_index|
+    n_index**p
+    p += 1
+  end
+  result = result.reduce(0) { |sum, i| sum + i }
+  (result % n).zero? ? result / n : -1
 end
