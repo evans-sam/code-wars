eb964011bcab55498f3c5e809b456436ad22eaa3
Sam Evans <evansst@icloud.com>
2020-07-13 09:04:35 -0600

    Refactors to best solution.

--- ruby/071320.rb
+++ ruby/071320.rb
@@ -1,13 +1,19 @@
-def disemvowel(str)
-  array = str.split(//)
-  new_array = []
-  vowels = ['a','e','i','o','u']
-  array.map do |letter|
-    new_array << letter if  !is_vowel_array(letter)  
-  end
-  new_array.join
-end
+require 'pry'
 
-def is_vowel_array(letter)
-  ["a", "e", "i", "o", "u"].include?(letter.downcase)
+# def disemvowel(str)
+#   array = str.split(//)
+#   new_array = []
+#   vowels = ['a','e','i','o','u']
+#   array.map do |letter|
+#     new_array << letter if  !is_vowel_array(letter)
+#   end
+#   new_array.join
+# end
+
+# def is_vowel_array(letter)
+#   ["a", "e", "i", "o", "u"].include?(letter.downcase)
+# end
+
+def disemvowel str
+  str.delete('aeiouAEIOU')
 end
