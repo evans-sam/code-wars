7c9b993bf12e0576236a530c6075969606f140cf
Sam Evans <evansst@icloud.com>
2020-07-14 09:23:44 -0600

    Adds solution to digitial_root.

--- js/071420.js
+++ js/071420.js
@@ -2,11 +2,16 @@
 function maskify(cc) {
   cc = cc.split("");
   for(i = 0; i + 4 < cc.length; i++) {
-    cc[i] = "#"
+    cc[i] = "#";
   }
-  return cc.join("")
+  return cc.join("");
 }
 
 function digital_root(n) {
-
+  var n_array = n.toString().split('').map(el=>parseInt(el));
+  while (n_array.length > 1) {
+    n = n_array.reduce((a, b) => a + b, 0);
+    var n_array = n.toString().split('').map(el=>parseInt(el));
+  }
+  return n;
 }
