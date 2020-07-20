e11d309a8b9bb67bc2c492560fa2e849c62c8cae
Sam Evans <evansst@icloud.com>
2020-07-20 09:53:30 -0600

    adds valid parentheses

--- js/072020.js
+++ js/072020.js
@@ -1,3 +1,12 @@
 function createPhoneNumber(numbers) {
   return `(${numbers.slice(0,3).join('')}) ${numbers.slice(4,7).join('')}-${numbers.slice(-4).join('')}`;
 }
+
+function validParentheses(parens) {
+    let count = 0;
+    for (i = 0; i < parens.length; i ++ ) {
+      parens.slice(i, i + 1) == '(' ? count++ : count--;
+      if (count < 0) return false;
+      }
+    return count === 0;
+}
