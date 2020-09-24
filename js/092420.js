43d1a524ea282a1a3b36922116b1fc1b9d349d93
Sam Evans <evansst@icloud.com>
2020-09-24 09:57:44 -0600

    Done.

--- js/092420.js
+++ js/092420.js
@@ -7,3 +7,17 @@ const isAnagram = (word1) => {
     return word1.split('').sort().join('') == word2.split('').sort().join('')
   }
 }
+
+var memo = [];
+
+function sum(n, m = n) {
+    if (n == 0) return 1;
+    if (n < 0 || m == 0) return 0;
+    if (memo[n] && memo[n][m]) return memo[n][m];
+    let total = sum(n, m - 1) + sum(n - m, m);
+    if (!memo[n]) {
+        memo[n] = [];
+    }
+    memo[n][m] = total;
+    return total;
+}
