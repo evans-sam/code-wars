05d3466e1aade4aa01a29208610386fb51fdd8e5
Sam Evans <evansst@icloud.com>
2020-10-20 10:40:28 -0600

    Done.

--- js/102020.js
+++ js/102020.js
@@ -1,4 +1,4 @@
-var isPP = function(n){
+const isPP = n =>{
   for(const divisor of divisors(n))
     for(let k = 2; k <= Math.log2(n); k++)
       if(divisor ** k == n) return [divisor, k]
@@ -14,3 +14,25 @@ const divisors = n => {
 
   return divisors
 }
+
+
+const comp = (array1, array2) => {
+
+  return array1 && array2 ? 
+    (
+      array1
+        .map(x => x * x)
+        .sort((x, y) => x - y)
+        .join('')
+    ) == (
+      array2
+        .sort((x, y) => x - y)
+        .join('')
+    ) : (
+      false
+    )
+}
+
+const a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+const a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+console.log(comp(a1, a2), true);
