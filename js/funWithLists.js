9c985b952ca291a398b335bd302953115f0631df
Sam Evans <evansst@icloud.com>
2020-12-03 12:40:29 -0700

    Done.

--- js/funWithLists.js
+++ js/funWithLists.js
@@ -1,3 +1,7 @@
 const map = (head, f) => (head ? new Node(f(head.data), map(head.next, f)) : head);
 
 const length = (head) => (head ? 1 + length(head.next) : 0);
+
+const countIf = (head, callback) => {
+  return head ? callback(head.data) + countIf(head.next, callback) : 0;
+};
