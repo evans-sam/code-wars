deb2b135f005169731ae3cc4c2612022a5eb94ea
Sam Evans <evansst@icloud.com>
2021-01-15 15:17:04 -0700

    Done.

---
+++ GetGrade.cs
@@ -0,0 +1,13 @@
+public class Kata
+{
+  public static char GetGrade(int s1, int s2, int s3)
+  {
+    int average = (s1 + s2 + s3) / 3;
+    if (average <= 100 && average >= 90) return 'A';
+    else if (average >= 80) return 'B';
+    else if (average >= 70) return 'C';
+    else if (average >= 60) return 'D';
+    else return 'F';
+  }
+
+}
