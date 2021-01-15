712401dd932e64bb26ba3dae53c22a6f17dd3536
Sam Evans <evansst@icloud.com>
2021-01-15 15:23:41 -0700

    Done.

--- GetGrade.cs
+++ GetGrade.cs
@@ -4,7 +4,7 @@ public class Kata
 {
   public static char GetGrade(int s1, int s2, int s3)
   {
-    int average = (s1 + s2 + s3) / 3;
+    double average = (s1 + s2 + s3) / 3;
     if (average <= 100 && average >= 90) return 'A';
     else if (average >= 80) return 'B';
     else if (average >= 70) return 'C';
---
+++ GetRealFloor.cs
@@ -0,0 +1,11 @@
+using System;
+
+public static class Kata 
+{
+  public static int GetRealFloor(int n) 
+  {
+    return  n <= 0 ? n :
+            n < 13 ? n - 1 :
+            n - 2;
+  }
+}
