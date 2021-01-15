9f00c47ee98715a5a1d540f8a2a689b28c3466fa
Sam Evans <evansst@icloud.com>
2021-01-15 15:09:58 -0700

    Done.

--- csharp/Numbers.cs
+++ csharp/Numbers.cs
@@ -6,4 +6,10 @@ public class Numbers
   {
     return Math.Round(number, 2);
   }
+
+  public static long? RangeSum(long n)
+  {
+    if (n <= 0 || n % 1 != 0) return null;
+    return(n * (n + 1)) / 2;
+  }
 }
