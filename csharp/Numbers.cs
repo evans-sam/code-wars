4c44a09f39c125a850a1cd0511b7725dc9f4ea84
Sam Evans <evansst@icloud.com>
2021-01-15 13:44:03 -0700

    Done.

---
+++ csharp/Numbers.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Numbers
+{
+  public static double TwoDecimalPlaces(double number)
+  {
+    return Math.Round(number, 2);
+  }
+}
---
+++ csharp/Watermelon.cs
@@ -0,0 +1,8 @@
+  using System;
+  public class Watermelon
+  {
+    public static bool Divide(int weight)
+    {
+       return (weight % 2 == 0) && weight > 2;
+    }
+}
