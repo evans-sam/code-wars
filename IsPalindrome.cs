e57cc0ebd233d98390266df1c697eb46853342f7
Sam Evans <evansst@icloud.com>
2021-01-15 15:32:27 -0700

    done.

---
+++ IsPalindrome.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Kata
+{
+  public static bool IsPalindrome(object line)
+  {
+    string line = "" + i;
+    char[] charArray = Array.Reverse(line.ToCharArray());
+    return s == new string(charArray);
+  }
+}
