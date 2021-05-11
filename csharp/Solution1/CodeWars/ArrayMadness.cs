69b547c8f090ffbc1ace0b44315f70bbec8ebcd1
Sam Evans <evansst@icloud.com>
2021-05-10 18:09:54 -0600

    Adds test project to sulution

--- csharp/Solution1/CodeWars/ArrayMadness.cs
+++ csharp/Solution1/CodeWars/ArrayMadness.cs
@@ -1,30 +1,35 @@
 using System.Linq;
-public partial class Kata
+
+namespace ArrayMadnessNamespace
 {
-  public static bool ArrayMadness(int[] a, int[] b)
+  public partial class Kata
   {
-    int a2 = 0;
-    int b3 = 0;
-    for(int i = 0; i < a.Length; i++)
+    public static bool ArrayMadness(int[] a, int[] b)
     {
-      a2 += a[i] * a[i];
+      int a2 = 0;
+      int b3 = 0;
+      for (int i = 0; i < a.Length; i++)
+      {
+        a2 += a[i] * a[i];
+      }
+
+      for (int i = 0; i < b.Length; i++)
+      {
+        b3 += b[i] * b[i] * b[i];
+      }
+
+      return a2 > b3;
     }
-    for(int i = 0; i < b.Length; i++)
+
+    public static bool FunctionalArrayMadness(int[] a, int[] b)
     {
-      b3 += b[i] * b[i] * b[i];
+      return a.Select((x) => x * x).Sum()
+             > b.Select((x) => x * x * x).Sum();
     }
 
-    return a2 > b3;
-  }
-
-  public static bool FunctionalArrayMadness(int[] a, int[] b)
-  {
-    return a.Select((x) => x * x).Sum()
-      > b.Select((x) => x * x * x).Sum();
-  }
-
-  public static bool RefactoredFunctionalArrayMadness(int[] a, int[] b)
-  {
-    return a.Sum((x) => x * x) > b.Sum((x) => x * x * x);
+    public static bool RefactoredFunctionalArrayMadness(int[] a, int[] b)
+    {
+      return a.Sum((x) => x * x) > b.Sum((x) => x * x * x);
+    }
   }
 }
