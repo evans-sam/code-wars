6acd6592b502d8be9108239d68f6c97494cbf726
Sam Evans <evansst@icloud.com>
2021-05-11 11:50:32 -0600

    adds connetc4 solution

---
+++ csharp/Solution1/CodeWars/Connect4.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace CodeWars
+{
+    public class Connect4
+    {
+        public Connect4()
+        {
+            GameBoard = new Board();
+        }
+
+        private bool GameIsOver;
+        private Player CurrentPlayer = Player.Red;
+        private Board GameBoard;
+
+        private enum Player
+        {
+            Empty = 0,
+            Red = 1,
+            Black = 2,
+        }
+
+        public string play(int column)
+        {
+            if (GameIsOver) return "Game has finished!";
+            try
+            {
+                GameBoard.Add(CurrentPlayer, column);
+                return GameBoard.HasWinner(CurrentPlayer)
+                    ? EndGame()
+                    : NextTurn();
+            }
+            catch (ArgumentException e)
+            {
+                return e.Message;
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        private string EndGame()
+        {
+            GameIsOver = true;
+            return $"Player {(int) CurrentPlayer} wins!";
+        }
+
+
+        private string NextTurn()
+        {
+            try
+            {
+                return $"Player {(int) CurrentPlayer} has a turn";
+            }
+            finally
+            {
+                CurrentPlayer = CurrentPlayer == Player.Red ? Player.Black : Player.Red;
+            }
+        }
+
+        private class Board
+        {
+            private const int Rows = 6;
+            private const int Columns = 7;
+            private Player[,] board { get; set; }
+
+            public Board()
+            {
+                NewBoard();
+            }
+
+            private void NewBoard()
+            {
+                board = new Player[Rows, Columns];
+                for (var row = 0; row < Rows; row++)
+                for (var column = 0; column < Columns; column++)
+                    board[row, column] = Player.Empty;
+            }
+
+            public void Add(Player disc, int column)
+            {
+                if (ColumnIsFull(column)) throw new ArgumentException("Column full!");
+
+                var row = 0;
+                while (row < Rows - 1 && board[row + 1, column] == Player.Empty)
+                    row++;
+
+                board[row, column] = disc;
+            }
+
+            private bool ColumnIsFull(int column) => board[0, column] != Player.Empty;
+
+            public bool HasWinner(Player disc)
+            {
+
+                for (var i = 0; i < Rows; i++)
+                for (var j = 0; j < Columns - 3; j++)
+                    if (board[i, j] == disc && board[i, j + 1] == disc && board[i, j + 2] == disc &&
+                        board[i, j + 3] == disc)
+                        return true;
+
+                for (var i = 0; i < Rows - 3; i++)
+                for (var j = 0; j < Columns; j++)
+                    if (board[i, j] == disc && board[i + 1, j] == disc && board[i + 2, j] == disc &&
+                        board[i + 3, j] == disc)
+                        return true;
+
+                for (var i = 3; i < Rows; i++)
+                for (var j = 0; j < Columns - 3; j++)
+                    if (board[i, j] == disc && board[i - 1, j + 1] == disc && board[i - 2, j + 2] == disc &&
+                        board[i - 3, j + 3] == disc)
+                        return true;
+
+                for (var i = 3; i < Rows; i++)
+                for (var j = 3; j < Columns; j++)
+                    if (board[i, j] == disc && board[i - 1, j - 1] == disc && board[i - 2, j - 2] == disc &&
+                        board[i - 3, j - 3] == disc)
+                        return true;
+                
+                return false;
+            }
+        }
+    }
+}
---
+++ csharp/Solution1/TestProject1/Connect4Tests.cs
@@ -0,0 +1,144 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    class Connect4Tests
+    {
+        private Connect4 test1 = new CodeWars.Connect4();
+        private Connect4 test2 = new CodeWars.Connect4();
+        private Connect4 test3 = new CodeWars.Connect4();
+        private Connect4 test4 = new CodeWars.Connect4();
+        
+        // Test 1
+        [TestCase]
+        public void BasicTest1a()
+        {
+            Assert.AreEqual("Player 1 has a turn", test1.play(0), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest1b()
+        {
+            Assert.AreEqual("Player 2 has a turn", test1.play(0), "Should return: 'Player 2 has a turn'");
+        }
+        
+        // Test 2
+        [TestCase]
+        public void BasicTest2a()
+        {
+            Assert.AreEqual("Player 1 has a turn", test2.play(0), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest2b()
+        {
+            Assert.AreEqual("Player 2 has a turn", test2.play(1), "Should return: 'Player 2 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest2c()
+        {
+            Assert.AreEqual("Player 1 has a turn", test2.play(0), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest2d()
+        {
+            Assert.AreEqual("Player 2 has a turn", test2.play(1), "Should return: 'Player 2 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest2e()
+        {
+            Assert.AreEqual("Player 1 has a turn", test2.play(0), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest2f()
+        {
+            Assert.AreEqual("Player 2 has a turn", test2.play(1), "Should return: 'Player 2 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest2g()
+        {
+            Assert.AreEqual("Player 1 wins!", test2.play(0), "Should return: 'Player 1 wins!'");
+        }
+        
+        // Test 3
+        [TestCase]
+        public void BasicTest3a()
+        {
+            Assert.AreEqual("Player 1 has a turn", test3.play(4), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest3b()
+        {
+            Assert.AreEqual("Player 2 has a turn", test3.play(4), "Should return: 'Player 2 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest3c()
+        {
+            Assert.AreEqual("Player 1 has a turn", test3.play(4), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest3d()
+        {
+            Assert.AreEqual("Player 2 has a turn", test3.play(4), "Should return: 'Player 2 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest3e()
+        {
+            Assert.AreEqual("Player 1 has a turn", test3.play(4), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest3f()
+        {
+            Assert.AreEqual("Player 2 has a turn", test3.play(4), "Should return: 'Player 2 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest3g()
+        {
+            Assert.AreEqual("Column full!", test3.play(4), "Should return: 'Column full!'");
+        }
+
+
+        // Test 4
+        [TestCase]
+        public void BasicTest4a()
+        {
+            Assert.AreEqual("Player 1 has a turn", test4.play(1), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest4b()
+        {
+            Assert.AreEqual("Player 2 has a turn", test4.play(1), "Should return: 'Player 2 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest4c()
+        {
+            Assert.AreEqual("Player 1 has a turn", test4.play(2), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest4d()
+        {
+            Assert.AreEqual("Player 2 has a turn", test4.play(2), "Should return: 'Player 2 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest4e()
+        {
+            Assert.AreEqual("Player 1 has a turn", test4.play(3), "Should return: 'Player 1 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest4f()
+        {
+            Assert.AreEqual("Player 2 has a turn", test4.play(3), "Should return: 'Player 2 has a turn'");
+        }
+        [TestCase]
+        public void BasicTest4g()
+        {
+            Assert.AreEqual("Player 1 wins!", test4.play(4), "Should return: 'Player 1 wins!'");
+        }
+        [TestCase]
+        public void BasicTest4h()
+        {
+            Assert.AreEqual("Game has finished!", test4.play(4), "Should return: 'Game has finished!'");
+        }
+
+    }
+}
