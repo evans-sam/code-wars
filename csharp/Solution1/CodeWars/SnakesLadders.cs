590a47c596439bcaa96d468cc1588920903963bc
Sam Evans <evansst@icloud.com>
2021-05-10 21:11:32 -0600

    Adds solution for Snakes and Ladders

--- csharp/Solution1/CodeWars/SnakesLadders.cs
+++ csharp/Solution1/CodeWars/SnakesLadders.cs
@@ -2,6 +2,102 @@
 {
     public class SnakesLadders
     {
-        
+        public SnakesLadders()
+        {
+            Player1 = new Player("1");
+            Player2 = new Player("2");
+            Shooter = Player1;
+        }
+
+        private Player Shooter;
+        private readonly Player Player1;
+        private readonly Player Player2;
+        private bool GameOver;
+
+        public string play(int die1, int die2)
+        {
+            try
+            {
+                if (GameOver) return "Game over!";
+                Shooter.Roll(die1, die2);
+                return Shooter.IsWinner()
+                    ? $"Player {Shooter.Name} Wins!"
+                    : $"Player {Shooter.Name} is on square {Shooter.Position}";
+            }
+            finally
+            {
+                NextTurn();
+            }
+        }
+
+        private void NextTurn()
+        {
+            if (Shooter.IsWinner()) GameOver = true;
+            if (Dice.IsDoubles) return;
+            Shooter = Shooter == Player1 ? Player2 : Player1;
+        }
+
+        private class Player
+        {
+            public Player(string name) => Name = name;
+            public string Name { get; }
+            public int Position { get; private set; }
+
+            public void Roll(int die1, int die2)
+            {
+                Dice.Roll(die1, die2);
+                Move();
+            }
+
+            public bool IsWinner() => Position == 100;
+
+            private void Move()
+            {
+                var newPosition = Position + Dice.Total;
+                Position = newPosition > 100 ? 100 - (newPosition - 100) : newPosition;
+                Position = SnakesOrLadders(Position);
+            }
+
+            private static int SnakesOrLadders(int space) =>
+                space switch
+                {
+                    2 => 38,
+                    7 => 14,
+                    8 => 31,
+                    15 => 26,
+                    16 => 6,
+                    21 => 42,
+                    28 => 84,
+                    36 => 44,
+                    46 => 25,
+                    49 => 11,
+                    51 => 67,
+                    62 => 19,
+                    64 => 60,
+                    71 => 91,
+                    74 => 53,
+                    78 => 98,
+                    87 => 94,
+                    89 => 68,
+                    92 => 88,
+                    95 => 75,
+                    99 => 80,
+                    _ => space,
+                };
+        }
+
+        private static class Dice
+        {
+            private static int _die1;
+            private static int _die2;
+            public static int Total => _die1 + _die2;
+            public static bool IsDoubles => _die1 == _die2;
+
+            public static void Roll(int die1, int die2)
+            {
+                _die1 = die1;
+                _die2 = die2;
+            }
+        }
     }
 }
--- csharp/Solution1/TestProject1/UnitTest1.cs
+++ csharp/Solution1/TestProject1/UnitTest1.cs
@@ -1,18 +1,39 @@
+using CodeWars;
 using NUnit.Framework;
 
 namespace TestProject1
 {
-    public class Tests
+    [TestFixture]
+    class SnakesLaddersTests
     {
-        [SetUp]
-        public void Setup()
+        private readonly SnakesLadders test = new SnakesLadders();
+
+        [TestCase]
+        public void BasicTest1()
         {
+            string result = test.play(1, 1);
+            Assert.AreEqual("Player 1 is on square 38",result, "Should return: 'Player 1 is on square 38'");
         }
 
-        [Test]
-        public void Test1()
+        [TestCase]
+        public void BasicTest2()
         {
-            Assert.Pass();
+            string result = test.play(1, 5);
+            Assert.AreEqual("Player 1 is on square 44",result, "Should return: 'Player 1 is on square 44'");
+        }
+
+        [TestCase]
+        public void BasicTest3()
+        {
+            string result = test.play(6, 2);
+            Assert.AreEqual("Player 2 is on square 31",result, "Should return: 'Player 2 is on square 31'");
+        }
+
+        [TestCase]
+        public void BasicTest4()
+        {
+            string result = test.play(1, 1);
+            Assert.AreEqual("Player 1 is on square 25", result, "Should return: 'Player 1 is on square 25'");
         }
     }
 }
