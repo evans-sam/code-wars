60a0ef37e713e417019ed5bd9279500f93f3d712
Sam Evans <evansst@icloud.com>
2021-09-14 16:43:43 -0600

    adds codility solution

---
+++ js/Codility.js
@@ -0,0 +1,16 @@
+﻿// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(S) {
+  const occurrences = Object.values(S
+    .split('')
+    .reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {}))
+    .sort((a, b) => b - a);
+  let exp = occurrences[0];
+  return occurrences.reduce((deletions, n) => {
+    if (n > exp) deletions += n - exp;
+    else exp = n;
+
+    return deletions;
+  }, 0);
+}
